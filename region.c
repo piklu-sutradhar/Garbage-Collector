496690c292f92cdbc8b822ba0de64873c5963541
Piklu Sutradhar <Piklu Sutradhar>
2017-04-02 16:28:17 -0500

    rinit function written

--- region.c
+++ region.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include <ctype.h>
 #include "regions.h"
-
+#include "linkedlist.h"
 
 //-------------------------------------------------------------------------------------
 // CONSTANTS and TYPES
 //-------------------------------------------------------------------------------------
-
+static Node *curr = NULL;
 //-------------------------------------------------------------------------------------
 // VARIABLES
 //-------------------------------------------------------------------------------------
@@ -20,27 +20,41 @@
 //-------------------------------------------------------------------------------------
 // FUNCTIONS
 //-------------------------------------------------------------------------------------
-Boolean rinit(const char *region_name, r_size_t region_size){
+Boolean rinit(const char *region_name, r_size_t region_size)
+{
+    Boolean rc = false;
+    if (region > 0)
+    {
+        region_size += 7;
+        region_size = region_size - (region_size % 8);
 
+        if (search(region_name) == false)
+        {
+            insert(region_name, region_size);
+            curr = firstNode();
+            rc = true;
+        }
+    }
+    return rc;
 }
-Boolean rchoose(const char *region_name){
-
+Boolean rchoose(const char *region_name)
+{
 }
-const char *rchosen(){
-
+const char *rchosen()
+{
 }
-void *ralloc(r_size_t block_size){
-
+void *ralloc(r_size_t block_size)
+{
 }
-r_size_t rsize(void *block_ptr){
-
+r_size_t rsize(void *block_ptr)
+{
 }
-Boolean rfree(void *block_ptr){
-
+Boolean rfree(void *block_ptr)
+{
 }
-void rdestroy(const char *region_name){
-
+void rdestroy(const char *region_name)
+{
 }
-void rdump(){
-    
+void rdump()
+{
 }
