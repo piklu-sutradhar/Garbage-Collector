3d6611c0cfaab837ce84da11e1efe3b2a9ec83d6
Piklu Sutradhar <Piklu Sutradhar>
2017-04-02 16:08:46 -0500

    header file of linkedlist created

---
+++ linkedlist.h
@@ -0,0 +1,13 @@
+typedef struct NODE Node;
+
+struct NODE
+{
+    char *name;
+    r_size_t size;
+    void *memoryRegion;
+    Node *next;
+}
+Boolean insert(const char *region_name, r_size_t region_size);
+Boolean search( char const * const target );
+char * firstItem();
+char * nextItem();
